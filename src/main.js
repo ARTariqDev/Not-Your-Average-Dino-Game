5f5988e8537a855c5e0ff68111fb791d59909b7e
ARTariqDev <tariqmahmood.dmp@gmail.com>
2026-07-24 18:34:56 +0500

    added jump functionality

--- src/main.js
+++ src/main.js
@@ -20,19 +20,44 @@ class BeginningScene extends Phaser.Scene {
         // Set bg color to white (for now)
         this.cameras.main.setBackgroundColor('#ffffff');
 
-        // Create the running animation using frames from the sheet
+        // dino animation
         this.anims.create({
             key: 'dino_run',
             frames: this.anims.generateFrameNumbers('dino_sheet', { start: 0, end:3  }),
-            frameRate: 10,
+            frameRate: 17,
             repeat: -1
         });
 
         // Add the sprite at X: 400, Y: 300 using frame 0
-        const dino = this.add.sprite(400, 300, 'dino_sheet', 0);
+        let dino = this.add.sprite(400, 300, 'dino_sheet', 0);
+
+
+        document.addEventListener('keydown', (event) => {
+        });
 
         const ground = this.add.sprite(400, 320, 'ground_sheet', 0);
+        let isJumping = false;
+
         
+
+        this.input.keyboard.on('keydown-SPACE', () => {
+            if (!isJumping) {
+                isJumping = true; // to prevent mid air space bar spam
+                dino.stop();
+                dino.setFrame(0);
+                this.tweens.add({
+                    targets: dino,
+                    y: dino.y - 100, // Jump height
+                    duration: 300, // Jump duration (in miliseconds)
+                    ease: 'Power2',
+                    yoyo: true, // Return to original position
+                    onComplete: () => {
+                        isJumping = false;
+                        dino.play('dino_run'); // resume animation
+                    }
+                });
+            }
+        });
         // Play the animation
         dino.play('dino_run');
     }
